4dc8337d634743ef9bc79d0794f4eb176eb27dc9
Anurag-545 <ranaasr60@gmail.com>
2017-02-06 16:28:53 +0530

    logo added

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Uploadbin</title>
+<title>Uploadbin-Upload Any Files</title>
 <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
 <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
 <!-- js -->
@@ -12,18 +12,18 @@
 <body>
 <div class="container-liquid">
 <div class="row" id="header">
-     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="logo">
+     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-7" id="logo">
       <img src="images/logo.png" id="logoimg"/><a href="index.php">Uploadbin</a>
-      </div>
-     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-     
-	 <div class="navbar-form navbar-left" role="search" id="search">
-  <a href="admin/"><button>Admin Login</button></a>
-
-</div>
-
-     </div>     
-</div>
-
+     </div>
+     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-5">
+          <div class="btn-group" role="group" aria-label="...">
+           <a href="admin/index.php">
+  	         <button type="button" class="btn btn-default" id="button">Admin Login</button>
+           </a>
+          </div> 
+        </div>     
+    </div>
 </div>
 </div>	
+</body>
+</html>
